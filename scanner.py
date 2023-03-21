34374af9321efa670741cc667df5e33ebe8e1968
j-janiszewski <jasiekj1@gmail.com>
2023-03-21 17:45:22 +0100

    grammar and parser added

--- scanner.py
+++ scanner.py
@@ -1,62 +1,83 @@
 import re
 from dataclasses import dataclass
+from enum import Enum
 
 
-# FIXME: background: url(watch?v=Ct6BUPvE2sM); couses an error
-TOKEN_PATTERNS = {
-    "PROPERTY_NAME": re.compile(r"[a-zA-Z\-]+:"),
-    "TEXT_VALUE": re.compile(r'".*"'),
-    "HEX_VALUE": re.compile(r"#[ABCDEF0-9]+"),
-    "REM_VAlUE": re.compile(r"[0-9]+em"),
-    "%_VALUE": re.compile(r"[0-9]+%"),
-    "INT_VALUE": re.compile(r"[0-9]+"),
-    "URL_VALUE": re.compile(r"url\([0-9a-zA-Z\?=_:;-]+\)"),
-    "OPERATOR": re.compile(r"\+|>"),
-    "ID": re.compile(r"\*|(#|\.)*[a-zA-Z._]*:*[a-zA-Z._]+"),
-    "IMPORTANT": re.compile(r"!important"),
-    "COMMENT": re.compile(r"\/\/[^\n]*"),
-    "SEMICOLON": re.compile(r";"),
-    "COMMA": re.compile(r","),
-    "SPACE": re.compile(r" "),
-    "NEWLINE": re.compile(r"\n"),
-    "INDENTATION": re.compile(r"\t+"),
-}
+class TokenNames(Enum):
+    PROPERTY_NAME = re.compile(r"[a-zA-Z\-]+: ")
+    REM_VAlUE = re.compile(r"[0-9]+rem")
+    URL_VALUE = re.compile(r"url\([0-9a-zA-Z\?=_:;-]+\)")
+    ID = re.compile(r"\*|(#|\.)*[a-zA-Z._]*:*[a-zA-Z._]+")
+    HEX_VALUE = re.compile(r"#[ABCDEFabcdefS0-9]{3}")
+    TEXT_VALUE = re.compile(r'".*"')
+    PERC_VALUE = re.compile(r"[0-9]+%")
+    PX_VALUE = re.compile(r"[1-9]+[0-9]*px")
+    INT_VALUE = re.compile(r"[0-9]+")
 
-IGNORED = ["SPACE", "COMMENT", "NEWLINE", "INDENTATION"]
+    OPERATOR = re.compile(r"\+|>")
+    IMPORTANT = re.compile(r"!important")
+    COMMENT = re.compile(r"\/\/[^\n]*")
+    SEMICOLON = re.compile(r";")
+    COMMA = re.compile(r",")
+    SPACE = re.compile(r" ")
+    NEWLINE = re.compile(r"\n")
+    INDENTATION = re.compile(r"\t+")
+    EOL = ""
+    INDENT = ""
+    DEDENT = ""
+
+
+IGNORED = [TokenNames.SPACE, TokenNames.COMMENT, TokenNames.INDENTATION]
+ADDED_BY_SCANNER = [TokenNames.EOL, TokenNames.INDENT, TokenNames.DEDENT]
 
 
 @dataclass
 class Token:
     name: str
     value: str
-    line_number: int
+    line: int
     column: int
+
+
+class UnexpectedTokenException(Exception):
+    def __init__(self, token, line, column) -> None:
+        super().__init__(
+            f" Unexpected token: `{token}` \n on line: {line}, column: {column}"
+        )
 
 
 class Scanner:
     def __init__(self, input_file) -> None:
-        self.tokens_read = []
+        self.tokens_read: list[Token] = []
         self.indent_level = 0
-        self.line_number = 0
-        self.column_number = 0
+        self.line = 1
+        self.column = 0
         while input_file:
             token_name, value, end = self.try_to_match(input_file)
             # if column_number is zero that means that we are starting to analyze new line of program
             # in this place i handle indentation
-            if self.column_number == 0:
+            if self.column == 0:
                 self.handle_indendations(token_name, value)
+            if len(self.tokens_read) != 0:
+                if (
+                    self.tokens_read[-1].name == TokenNames.INDENT
+                    or self.tokens_read[-1].name == TokenNames.DEDENT
+                ) and token_name == TokenNames.SPACE:
+                    raise IndentationError(
+                        f"Only tabs can be used for indendation, Whitespace character on line: {self.line}, column: {self.column}"
+                    )
 
             if token_name not in IGNORED:
                 self.add_new_token(token_name, value)
-            if token_name == "NEWLINE":
-                self.line_number += 1
-                self.column_number = 0
+            if token_name == TokenNames.NEWLINE:
+                self.line += 1
+                self.column = 0
             else:
-                self.column_number += end
+                self.column += end
             input_file = input_file[end:]
         else:
-            self.handle_indendations("EOF", "")
-            self.add_new_token("EOF", "")
+            self.handle_indendations(TokenNames.EOL, "")
+            self.add_new_token(TokenNames.EOL, "")
 
     def try_to_match(self, input):
         """
@@ -69,13 +90,17 @@ class Scanner:
         Returns:
             tuple[str, str, int]: tuple with token_name, token_value and ending column number
         """
-        for token_name, regex in TOKEN_PATTERNS.items():
-            match = regex.match(input)
+        for token_name in TokenNames:
+            if token_name in ADDED_BY_SCANNER:
+                continue
+            match = token_name.value.match(input)
             if match:
+                # handling case when begging of hex i matched with ID
+                if token_name == TokenNames.ID and len(input) != match.end():
+                    if input[match.end()].isdigit():
+                        continue
                 return token_name, match.group(), match.end()
-        raise RuntimeError(
-            f"Error: Unexpected character {input} on line {self.line_number}"
-        )
+        raise UnexpectedTokenException(input.split()[0], self.line, self.column)
 
     def handle_indendations(self, token_name, token_value):
         """
@@ -86,11 +111,16 @@ class Scanner:
             token_name (str): current Token name
             token_value (str): current Token value
         """
-        if token_name == "INDENTATION":
+        if token_name == TokenNames.SPACE:
+            raise IndentationError(
+                f"Only tabs can be used for indendation, Whitespace character on line: {self.line}, column: {self.column}"
+            )
+        if token_name == TokenNames.INDENTATION:
             number_of_tabs = token_value.count("\t")
+            self.column += number_of_tabs
             if number_of_tabs > self.indent_level:
                 for _ in range(number_of_tabs - self.indent_level):
-                    self.add_new_token("INDENT", "indentation")
+                    self.add_new_token(TokenNames.INDENT, "indentation")
                     self.indent_level += 1
             elif number_of_tabs < self.indent_level:
                 self.dedent_n_times(self.indent_level - number_of_tabs)
@@ -104,7 +134,7 @@ class Scanner:
             n (int): number of DEDENT's
         """
         for _ in range(n):
-            self.add_new_token("DEDENT", "dedentation")
+            self.add_new_token(TokenNames.DEDENT, "dedentation")
             self.indent_level -= 1
 
     def add_new_token(self, token_name, token_value):
@@ -114,6 +144,4 @@ class Scanner:
             token_name (str): new Token name
             token_value (str): new Token value
         """
-        self.tokens_read.append(
-            Token(token_name, token_value, self.line_number, self.column_number)
-        )
+        self.tokens_read.append(Token(token_name, token_value, self.line, self.column))
